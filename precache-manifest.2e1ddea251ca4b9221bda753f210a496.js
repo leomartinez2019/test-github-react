731aa453098950b3c5bb16514f269ce923c572f6
leomartinez2019 <lowenhard@yahoo.com>
2019-09-08 20:48:38 -0500

    Updates

---
+++ precache-manifest.2e1ddea251ca4b9221bda753f210a496.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a37b57dabfe25806ca5",
+    "url": "/test-github-react/static/css/main.70707dbe.chunk.css"
+  },
+  {
+    "revision": "1a37b57dabfe25806ca5",
+    "url": "/test-github-react/static/js/main.912f61cf.chunk.js"
+  },
+  {
+    "revision": "75637b403e398fca548e",
+    "url": "/test-github-react/static/js/runtime~main.4b53129a.js"
+  },
+  {
+    "revision": "d7ede6da5778c87c623d",
+    "url": "/test-github-react/static/js/2.8e1c5d7c.chunk.js"
+  },
+  {
+    "revision": "380d0f80627f3220075c41ae95ecfa49",
+    "url": "/test-github-react/static/media/AWS_Certification.380d0f80.png"
+  },
+  {
+    "revision": "aa6eb2bdefc33d90b698fcc8c1cdd443",
+    "url": "/test-github-react/static/media/SolutionsArchitect.aa6eb2bd.pdf"
+  },
+  {
+    "revision": "8655969ae9a8a4449f053061060bbeda",
+    "url": "/test-github-react/static/media/django-snapshot.8655969a.png"
+  },
+  {
+    "revision": "4c6e68c233fc80f573ced80befae4289",
+    "url": "/test-github-react/static/media/blog.4c6e68c2.png"
+  },
+  {
+    "revision": "7a558cbf053d8e140eface7db1f515ad",
+    "url": "/test-github-react/static/media/mapas.7a558cbf.png"
+  },
+  {
+    "revision": "e946c17c81a201f47869afee98e50645",
+    "url": "/test-github-react/static/media/portafolio.e946c17c.png"
+  },
+  {
+    "revision": "0f71ecec353b6ad5be4f148941f86d7a",
+    "url": "/test-github-react/static/media/home.0f71ecec.jpg"
+  },
+  {
+    "revision": "a2e210f179ab48383b528a248e64a201",
+    "url": "/test-github-react/index.html"
+  }
+];
